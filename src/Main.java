461a5db4ac206d02c8d8bc671a8dc780bc65c504
Emma <emmayang78@gmail.com>
2017-07-19 14:24:40 +0800

    Added comments and title

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,3 @@
-//package sample;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//public class Main extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
-
-package sample;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -36,38 +10,44 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+/**
+ * Uses JavaFX to create a simple sum calculator
+ */
+
 public class Main extends Application {
 
-    Label result = new Label();
+    TextField result = new TextField();
     TextField oneField = new TextField();
     TextField twoField = new TextField();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        primaryStage.setTitle("Simple Calculator");
+
         GridPane rootNode = new GridPane();
         rootNode.setHgap(5);
         rootNode.setVgap(5);
         rootNode.setPadding(new Insets(30));
 
-//        rootNode.getChildren().add(0,0, new Label("Hi"));
-
-        rootNode.add(new Label("1: "), 0,2);
+        rootNode.add(new Label("First Value: "), 0,2);
         rootNode.add(oneField, 1,2);
 
-        rootNode.add(new Label("2: "), 0,3);
+        rootNode.add(new Label("Second Value: "), 0,3);
         rootNode.add(twoField, 1,3);
 
 
         Button calculate = new Button("Calculate");
         rootNode.setHalignment(calculate, HPos.RIGHT);
-        rootNode.add(calculate, 1, 5);
+        rootNode.add(calculate, 1, 6);
 
-        rootNode.add(result, 1, 6);
+        CalculateHandler calculateHandler = new CalculateHandler();
+        calculate.setOnAction(calculateHandler);
 
+        rootNode.add(new Label("Sum: "), 0,5);
+        rootNode.add(result, 1,5);
 
-//        Scene scene = new Scene(rootNode, 240, 100);
-        primaryStage.setScene(new Scene(rootNode, 300, 275));
+        primaryStage.setScene(new Scene(rootNode, 350, 200));
         primaryStage.show();
 
     }
